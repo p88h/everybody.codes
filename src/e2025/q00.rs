5f7bd0ed008e7395d177679236fb2dba85244a40
Staszek Pasko <staszek@gmail.com>
2025-11-08 10:50:45 +0100

    template

---
+++ src/e2025/q00.rs
@@ -0,0 +1,11 @@
+pub fn part1(input: &str) -> String {
+    input.to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    input.to_string()
+}
+
+pub fn part3(input: &str) -> String {
+    input.to_string()
+}
