73bbac6275e35b3e65d66f3c95a469093aead03b
Staszek Pasko <staszek@gmail.com>
2025-11-08 12:25:20 +0100

    simplify

--- src/e2025/q02.rs
+++ src/e2025/q02.rs
@@ -1,11 +1,11 @@
 use num_complex::Complex;
 use rayon::prelude::*;
 
-pub fn cycle(num: &Complex<i64>, times: i32) -> Complex<i64> {
+pub fn cycle1(num: &Complex<i64>) -> Complex<i64> {
     let mut result = Complex::new(0, 0);
-    for _ in 0..times {
+    for _ in 0..3 {
         result = result * result;
-        result = Complex::new(result.re / 10, result.im / 10);
+        result = result / 10;
         result = result + num;
     }
     result
@@ -20,21 +20,21 @@ pub fn read_complex(input: &str) -> Complex<i64> {
 
 pub fn part1(input: &str) -> String {
     let c = read_complex(input);
-    let res = cycle(&c, 3);
+    let res = cycle1(&c);
     format!("[{},{}]", res.re, res.im)
 }
 
-pub fn cycle2(num: &Complex<i64>, times: i32) -> i32 {
+pub fn cycle2(num: &Complex<i64>) -> i32 {
     let mut result = Complex::new(0, 0);
-    for d in 0..times {
+    for d in 0..100 {
         result = result * result;
-        result = Complex::new(result.re / 100000, result.im / 100000);
+        result = result / 100000;
         result = result + num;
         if result.re.abs() > 1000000 || result.im.abs() > 1000000 {
             return d;
         }
     }
-    times
+    100
 }
 
 pub fn part2(input: &str) -> String {
@@ -43,7 +43,7 @@ pub fn part2(input: &str) -> String {
     for x in 0..101 {
         for y in 0..101 {
             let test_c = Complex::new(c.re + x * 10, c.im + y * 10);
-            if cycle2(&test_c, 100) == 100 {
+            if cycle2(&test_c) == 100 {
                 total += 1;
             }
         }
@@ -59,7 +59,7 @@ pub fn part3(input: &str) -> String {
             let mut local_total = 0;
             for y in 0..1001 {
                 let test_c = Complex::new(c.re + x, c.im + y);
-                if cycle2(&test_c, 100) == 100 {
+                if cycle2(&test_c) == 100 {
                     local_total += 1;
                 }
             }
@@ -80,17 +80,17 @@ mod tests {
 
     #[test]
     fn test_cycle2() {
-        assert_eq!(cycle2(&Complex::new(35630, -64880), 100), 100);
-        assert_eq!(cycle2(&Complex::new(35630, -64870), 100), 100);
-        assert_eq!(cycle2(&Complex::new(35640, -64860), 100), 100);
-        assert_eq!(cycle2(&Complex::new(36230, -64270), 100), 100);
-        assert_eq!(cycle2(&Complex::new(36250, -64270), 100), 100);
+        assert_eq!(cycle2(&Complex::new(35630, -64880)), 100);
+        assert_eq!(cycle2(&Complex::new(35630, -64870)), 100);
+        assert_eq!(cycle2(&Complex::new(35640, -64860)), 100);
+        assert_eq!(cycle2(&Complex::new(36230, -64270)), 100);
+        assert_eq!(cycle2(&Complex::new(36250, -64270)), 100);
 
-        assert_eq!(cycle2(&Complex::new(35460, -64910), 100), 26);
-        assert_eq!(cycle2(&Complex::new(35470, -64910), 100), 27);
-        assert_eq!(cycle2(&Complex::new(35480, -64910), 100), 29);
-        assert_eq!(cycle2(&Complex::new(35680, -64850), 100), 94);
-        assert_eq!(cycle2(&Complex::new(35630, -64830), 100), 99);
+        assert_eq!(cycle2(&Complex::new(35460, -64910)), 26);
+        assert_eq!(cycle2(&Complex::new(35470, -64910)), 27);
+        assert_eq!(cycle2(&Complex::new(35480, -64910)), 29);
+        assert_eq!(cycle2(&Complex::new(35680, -64850)), 94);
+        assert_eq!(cycle2(&Complex::new(35630, -64830)), 99);
     }
 
     #[test]
